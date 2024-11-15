4be1376a88ae5248c50bece6ffc4f7a2e46c8534
Robert Sparks <robert.william.sparks@gmail.com>
2024-11-15 17:20:55 +0000

    build system change

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
-  description = "Logo Animation v2";
-
+  description = "Logo Animation Development";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
@@ -18,45 +16,7 @@
             --minify \
             --outfile=static/js/script.js
         '';
-        
-        watchJs = pkgs.writeShellScriptBin "watch-js" ''
-          ${pkgs.esbuild}/bin/esbuild \
-            static/js/src/main.ts \
-            --bundle \
-            --watch \
-            --outfile=static/js/script.js
-        '';
-        
-        dev = pkgs.writeShellScriptBin "dev" ''
-          ${buildJs}/bin/build-js
-          ${pkgs.zola}/bin/zola serve --interface 0.0.0.0
-        '';
-        
-        build = pkgs.writeShellScriptBin "build" ''
-          ${buildJs}/bin/build-js
-          ${pkgs.zola}/bin/zola build
-        '';
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            zola
-            nodejs
-            typescript
-            esbuild
-            typescript-language-server
-            buildJs
-            watchJs
-            dev
-            build
-          ];
-
-          shellHook = ''
-            echo "🚀 Development environment ready!"
-            echo "Commands: dev, build, build-js, watch-js"
-          '';
-        };
-
+      in {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "logo-animation";
           src = ./.;
@@ -65,17 +25,19 @@
             zola
             nodejs
             esbuild
+            typescript
           ];
-
+          
           buildPhase = ''
             ${buildJs}/bin/build-js
             ${pkgs.zola}/bin/zola build
           '';
 
-          installPhase = ''
-            cp -r public $out
+	  installPhase = ''
+            mkdir -p $out
+            cp -r public/* $out/
           '';
+          
         };
-      }
-    );
+      });
 }
--- public/js/src/types.ts
+++
@@ -1,4 +0,0 @@
-export type Config = {
-    numberOfLines: number;
-    angle: number;
-};
